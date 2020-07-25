e98e6b1c666fa52b99d9241fbd09ac57552d5ad0
Lukeg32 <lgantar19@georgefox.edu>
2020-07-25 12:57:51 -0700

    c++

---
+++ networking/networkConfig.hpp
@@ -0,0 +1,14 @@
+/*
+ * store all network constants here,
+ * or elsewhere once integrated into
+ * the main code, ikc
+ *
+ */
+
+#define PORT  12345
+#define SUPERSECRETKEY_SERVER "ndcayfserver"
+#define SUPERSECRETKEY_CLIENT "ndcayfclient"
+#define MAXSERVERS 5
+
+// not sure if this value is important enough
+#define BUFSIZE 2048
