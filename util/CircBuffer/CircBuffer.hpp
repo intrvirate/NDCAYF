90889c0f77d9388416df323bb2f2d33555a65267
Kevin Lockwood <kevin-b-lockwood@gmail.com>
2021-01-28 23:44:22 -0800

    CircBuffer: Cleaned up header file of @lukeg32's cancerous cruft

--- util/CircBuffer/CircBuffer.hpp
+++ util/CircBuffer/CircBuffer.hpp
@@ -9,15 +9,7 @@ class CircBuffer {
   public:
     CircBuffer(int packet_size, int packet_count, int fetch_size);
     int add(char *data);
-    //bool canAddMore();
-    //bool needMore();
-    //bool clearing();
-    //bool isNextReady();
-    //bool isFull();
     char* getData();
-    //int getSize();
-    //void noMore();
-    //int qSize();
     void destroy();
 
   private:
@@ -29,7 +21,6 @@ class CircBuffer {
     uintptr_t _fetch_size;
     char* _buffer;
     char* _output;
-    //bool clear;
 };
 
 #endif
