e1d4a8969db14fe4cc8ad63485502f2479349eb4
Kevin Lockwood <kevin-b-lockwood@gmail.com>
2021-04-04 23:12:06 -0700

    Browser: is back to where we started, thank the lord. Now oop and everything

--- util/browser/Browser.hpp
+++ util/browser/Browser.hpp
@@ -1,6 +1,8 @@
 #ifndef BROWSER_H
 #define BROWSER_H
 
+#define INPUT_SIZE 256
+
 #include <string>
 #include <vector>
 #include "util/imgui/imgui.h"
@@ -20,6 +22,14 @@ class Browser {
         Browser(std::string start_dir);
 
         /**
+         * Creates a Browser in the given directory
+         *
+         * @param start_dir     the directory to start in
+         * @param can_cancel    whether or not this Browser can be dismissed
+         */
+        Browser(std::string start_dir, bool can_cancel);
+
+        /**
          * Returns true if this Browser has selected and committed to a path
          *
          * Like, has clicked "select"
@@ -29,16 +39,65 @@ class Browser {
         bool hasSelected() const;
 
         /**
+         * Sets the ability for the user to cancel selection
+         *
+         * @param can_cancel    true if can cancel
+         */
+        void setCanCancel(bool can_cancel);
+
+        /**
+         * Returns true if this Browser can be canceled
+         *
+         * @return true if this Browser can be canceled
+         */
+        bool canCancel() const;
+
+        /**
+         * Returns true if this Browser has been canceled
+         *
+         * @return true if this Browser has been canceled
+         */
+        bool hasCanceled() const;
+
+        /**
          * Draws this Browser on the screen
          */
         void draw();
 
         /**
-         * Returns a copy of the path selected by this browser
+         * Returns a copy of the path to the filename selected by this browser
+         *
+         * @return a string representation of the selection from this Browser
+         */
+        std::string getSelectionPath();
+
+        /**
+         * Returns a copy of the filename selected by this browser
          *
          * @return a string representation of the selection from this Browser
          */
         std::string getSelection();
+
+        /**
+         * Sets the path of this Browser to a new path
+         *
+         * @param path  the path, '/' delimited.
+         */
+        void setSelection(std::string path);
+
+        /**
+         * Returns the path that this browser is on
+         *
+         * @return a string representation of the working dir of this browser
+         */
+        std::string getPath();
+
+        /**
+         * Sets the current path of this Browser
+         *
+         * @param path  the path, '/' delimited.
+         */
+        void setPath(std::string path);
 
     private:
         /**
@@ -55,7 +114,11 @@ class Browser {
 
         // State vars
         std::string _current_path;
+        std::string _current_selection;
+        char _input[INPUT_SIZE];
         bool _has_selected;
+        bool _can_cancel;
+        bool _has_canceled;
 
         // A cache of the files on this level
         std::vector<std::string> _files;
@@ -63,7 +126,6 @@ class Browser {
 
         // Magic numbers
         char delimiter = '/';
-
 };
 
 #endif
